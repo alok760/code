7976249c1cfa24559d11ed2412a0a79df39b3e7a
Alok Kumar <alok760@gmail.com>
2018-04-13 15:00:00 -0530

    A very Big Sum problem in python3

---
+++ hackerank/Algorithms-Warmup/A Very Big Sum/AVeryBigSum-Python.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import os
+import sys
+
+def aVeryBigSum(n, ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(n, ar)
+
+    f.write(str(result) + '\n')
+
+    f.close()
