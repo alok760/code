3ae2bb1f2e9fd6e0eecf935ad678ac8ea18cd7f4
alok <alok760@gmail.com>
2019-01-17 02:13:07 +0530

    solution

--- codechef/LECANDY/solution.cpp
+++ codechef/LECANDY/solution.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-  cout<<"hello";
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    long int n,k,sum=0,temp;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+      cin>>temp;
+      sum+=temp;
+    }
+  cout<<(sum>k?"No":"Yes")<<endl;
+
+  }
   return 0;
 }
