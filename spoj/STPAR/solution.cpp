1bf194707267c2e7bbaf4c9478d7c6b37604b308
alok <alok760@gmail.com>
2019-01-18 01:59:36 +0530

    solution

---
+++ spoj/STPAR/solution.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  while(n!=0)
+  {
+    int count = 1, ocount = INT_MAX;
+    for(int i=0;i<n;i++)
+      {
+        cin>>temp;
+        if(temp == count)
+          count++;
+        else
+          if(temp > ocount)
+      }
+
+  }
+  return 0;
+}
