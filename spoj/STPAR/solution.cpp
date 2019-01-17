4265a5f90a685a240783886b2190c40afd476e98
alok <alok760@gmail.com>
2019-01-18 02:16:30 +0530

    solution

--- spoj/STPAR/solution.cpp
+++ spoj/STPAR/solution.cpp
@@ -6,16 +6,30 @@ int main()
   cin>>n;
   while(n!=0)
   {
-    int count = 1, ocount = INT_MAX;
+    int count = 1, ocount = 69,temp;
     for(int i=0;i<n;i++)
       {
         cin>>temp;
         if(temp == count)
           count++;
+
+        else if(temp > ocount)
+        {
+          cout<<"no"<<endl;
+          break;
+        }
         else
-          if(temp > ocount)
+          ocount=temp;
+
+        cout<<"debug : "<<temp<<" "<<i<<" "<<count<<" "<<ocount<<endl;
+
+        if(i==n-1)
+          cout<<"yes"<<endl;
       }
 
+      cin>>n;
+
   }
+  cout<<"end";
   return 0;
 }
